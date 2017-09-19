b28b3cdb1e7206ddcd60af0a00abfb06b03017e7
Amit K Biswas <biswas1992@hotmail.com>
2017-09-19 14:18:27 -0400

    First commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>@yield('title')</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+
+@yield('styles')
+</head>
+<body>
+	
+    @yield('content')
+
+<script
+  src="https://code.jquery.com/jquery-1.12.4.min.js"
+  integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
+  crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+@yield('scripts')
+
+</body>
+</html>
---
+++ resources/views/shop/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+
+@section('title')
+Shopping Cart
+@endsection
+
+@section('content')
+
+<h2>Hello Its Works. Now I am working with shopping cart</h2>
+
+@endsection
+
