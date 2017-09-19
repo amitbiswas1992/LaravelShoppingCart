1103f45b7c7c189af33b552498087375328c16b2
Amit K Biswas <biswas1992@hotmail.com>
2017-09-19 18:07:45 -0400

    image and button fixed

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -11,7 +11,7 @@ Shopping Cart
   <div class="card-block">
     <h4 class="card-title">Card title</h4>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div class="pull-left price"> $12</div> 
     <a href="#" class="btn btn-outline-primary pull-right" role="button">Button</a></p>
   </div>
 </div>
